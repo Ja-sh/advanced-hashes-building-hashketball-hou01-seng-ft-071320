9ce283291b66336bf1bcbb829128e0cd1e30d8f1
Joshua Moore <morejoshua99@hotmail.com>
2020-06-22 21:15:00 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,24 +1,4 @@
 def game_hash
-  {
-  :home => {
-    :team_name  => {:colors},
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  :away => {
-    :team_name  => {:colors}}
-  }
+{}
   
 end
