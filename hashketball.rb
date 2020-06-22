edf14f81b1c26fab38507563e678bea516d23cda
Joshua Moore <morejoshua99@hotmail.com>
2020-06-22 21:30:02 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,10 +1,11 @@
 def game_hash
   {
     :home => {
+      :team_name  => "Brooklyn Nets",
       
     },
     :away => {
-      :team_name => "Charlotte Hornets"
+      :team_name => "Charlotte Hornets",
       
     }
   }
