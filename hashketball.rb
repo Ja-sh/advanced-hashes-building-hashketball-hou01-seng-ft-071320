6be07139448390dd0d905386b2b5d708daa3aac6
Joshua Moore <morejoshua99@hotmail.com>
2020-06-22 21:34:52 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -6,7 +6,12 @@ def game_hash
       :players => [
         
           :player_name => "Alan Anderson",
-          :number => 0
+          :number => 0,
+          :shoe => 16,
+          :points => 22,
+          :rebounds => 12,
+          :assists => 12,
+          
       ]
       
       
